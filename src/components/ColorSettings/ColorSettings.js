717da672a288d96b95ddefe81b8b7669800706d7
SanyaSukhariev <sukhariev0212@gmail.com>
2021-02-18 14:47:04 +0200

    add component ColorSettings

--- src/components/ColorSettings/ColorSettings.js
+++ src/components/ColorSettings/ColorSettings.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 
-const ColorSettings =()=>{
+const ColorPicker =()=>{
     return(
         <div>
 
@@ -9,4 +9,4 @@ const ColorSettings =()=>{
     )
 }
 
-export default ColorSettings
+export default ColorPicker
