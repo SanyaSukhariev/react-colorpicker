88976d11325f9f1a8de4784ed1b3c5305b459195
SanyaSukhariev <sukhariev0212@gmail.com>
2021-02-18 14:49:04 +0200

    changed name

--- src/components/ColorPicker/ColorPicker.js
+++ src/components/ColorPicker/ColorPicker.js
@@ -1,10 +1,11 @@
 import React from "react";
 
 
+
 const ColorPicker =()=>{
     return(
         <div>
-
+                
         </div>
     )
 }
--- src/components/ColorSettings/ColorSettings.js
+++ src/components/ColorSettings/ColorSettings.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 
-const ColorPicker =()=>{
+const ColorSettings =()=>{
     return(
         <div>
 
@@ -9,4 +9,4 @@ const ColorPicker =()=>{
     )
 }
 
-export default ColorPicker
+export default ColorSettings
